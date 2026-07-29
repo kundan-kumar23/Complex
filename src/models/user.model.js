ab0f5ebc9fea261e1a6520a794349f927a32f2d5
kundan-kumar23 <kundankumar777faridkot@gmail.com>
2026-07-29 18:53:43 +0530

    created user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -56,11 +56,11 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-userSchema.method.isPasswordCorrect = async function (password) {
+userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
-userSchema.method.generateAcessToken = function () {
+userSchema.methods.generateAcessToken = function () {
   return jwt.sign(
     {
       _id: this._id,
@@ -74,7 +74,7 @@ userSchema.method.generateAcessToken = function () {
     }
   );
 };
-userSchema.method.generateRefreshToken = function () {
+userSchema.methods.generateRefreshToken = function () {
     return jwt.sign(
     {
       _id: this._id,
