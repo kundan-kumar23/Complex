962c83ca4ab2545c12d3caf6a5d6519a419ca533
kundan-kumar23 <kundankumar777faridkot@gmail.com>
2026-07-29 17:29:02 +0530

    added required middlewares in app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
 require('dotenv').config()
+
+
+app.use(cors());
+app.use(cookieParser())
+app.use(express.json({limit:"16kb"}))
+app.use(express.static("public"))
+app.use(express.urlencoded({extended : true,limit:"16kb"}))
 
 module.exports = app
