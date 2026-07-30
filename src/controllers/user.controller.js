3f033fdd675549f8e9020917f6920b7c401533de
kundan-kumar23 <kundankumar777faridkot@gmail.com>
2026-07-31 00:12:04 +0530

    added register logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,85 @@
+const User = require("../models/user.model")
+const { uploadOnCloudinary } = require("../utils/cloudinary")
+
 const register = async (req,res)=>
 {
     try{
-        res.status(200).json({
-            sucess:"ok"
+       let {username,fullname,email,password} = req.body
+      if([username,fullname,email,password].some((e)=>e?.trim() == ""))
+      {
+        return res.status(400).json({
+            error : "Please fill all required field"
         })
+      }
+
+      let existed_user =await User.findOne({
+        $or:[
+            { username },
+            { email }
+        ]
+    }
+      )
+
+      if(existed_user)
+      {
+         return res.status(400).json({
+            error : "user already exist"
+        })
+      }
+
+     const avatarLocalPath = req.files?.avatar[0]?.path;
+     const coverImageLocalPath =req.files?.coverImage[0]?.path;
+
+     if(!avatarLocalPath)
+     {
+         return res.status(400).json({
+            error : "missing avatar image"
+        })
+     }
+
+     let avatar = await uploadOnCloudinary(avatarLocalPath)
+     if(coverImageLocalPath)
+     {
+        let coverImage = await uploadOnCloudinary(coverImageLocalPath);
+     }
+     
+
+     if(!avatar)
+     {
+         return res.status(500).json({
+            error : "internal server error"
+        })
+     }
+
+     let user = await User.create({
+        username: username.toLowerCase(),
+        avatar : avatar.url,
+        coverImage : coverImage?.url || "",
+        password,
+        email,
+        fullname
+     })
+
+     const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+     )
+
+     if(!createdUser)
+     {
+         return res.status(500).json({
+            error : "internal server error"
+        })
+     }
+
+     return res.status(200).json({
+        success : "sucessfully registered",
+        data : createdUser
+     })
+    
     }catch(e){
-        console.log(e)
+       return res.status(500).json({
+        error : "internal server error"
+       })
     }
 }
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,20 @@
 const express = require("express")
-const register = require("../controllers/user.controller")
+const register = require("../controllers/user.controller");
+const { upload } = require("../middlewares/multer.middleware");
 const router = express.Router()
 
-router.post("/register",register);
+router.post("/register",upload.fields(
+    [
+        {
+            name : "avatar",
+            maxCount: 1
+        },
+        {
+            name : "coverImage",
+            maxCount : 1
+        }
+    ]
+),register);
 
 
 module.exports = router;
