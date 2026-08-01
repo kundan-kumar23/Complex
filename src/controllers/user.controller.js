68e529821cd6373761be67d3029b01a20eb1c692
kundan-kumar23 <kundankumar777faridkot@gmail.com>
2026-08-01 12:11:10 +0530

    completed register feature with uploading at cloudinary

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -28,7 +28,13 @@ const register = async (req,res)=>
       }
 
      const avatarLocalPath = req.files?.avatar[0]?.path;
-     const coverImageLocalPath =req.files?.coverImage[0]?.path;
+    //  const coverImageLocalPath =req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0)
+    {
+        coverImageLocalPath =req.files.coverImage[0].path
+    }
 
      if(!avatarLocalPath)
      {
